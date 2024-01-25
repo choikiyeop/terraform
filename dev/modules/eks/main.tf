a09688de53a64c91268753bb229aa35dd985aea8
choikiyeop <cky9709@naver.com>
2024-01-25 17:40:48 +0900

    쿠버네티스 버전 1.28 -> 1.29 업데이트

--- dev/modules/eks/main.tf
+++ dev/modules/eks/main.tf
@@ -3,7 +3,7 @@ module "eks" {
   version = "19.21.0"
 
   cluster_name = var.cluster_name
-  cluster_version = "1.28"
+  cluster_version = "1.29"
   cluster_endpoint_public_access = true
 
   cluster_addons = {
